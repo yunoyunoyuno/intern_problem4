af45562b243f69cedccc67d6b6ae100a5bb44c2b
yn <Moreseus@outlook.com>
2021-03-03 22:59:58 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cheerio = require('cheerio');
+const axios = require('axios');
+const app = express();
+
+img = [];
+
+const scrap = async () => {
+    
+        const { data } = await axios.get('https://theinternship.io/');
+        let $ = cheerio.load(data);
+
+        $('.center-logos').each(function(i,_){
+            if($(this)['0'] && $(this)['0'].attribs)
+                image = `${$(this)['0'].attribs.src}`;
+            img[i] = {img : image}
+        })
+        $('.list-company').each(function(i,_){
+            if($(this)['0'] && $(this)['0'].children[0])
+                text = `${$(this)['0'].children[0].data}`
+                img[i].size = text.length
+                img[i].data = text
+        })
+        img.sort((a,b) => a.size - b.size)
+   
+};
+
+app.use('/',async (req,res,next)=>{
+    await scrap();
+    result = img.map(e =>  {
+        return {logo : `https://theinternship.io/${e.img}`}
+    });
+    res.status(200).send({
+        companies : result
+    })
+})
+
+
+app.listen(3000,()=>{
+    console.log("Start server on port 3000")
+})
