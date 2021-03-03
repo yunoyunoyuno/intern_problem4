e916e494e0789991c106dae187b7899616891d69
yn <Moreseus@outlook.com>
2021-03-03 23:08:22 +0700

    add little error handling

--- index.js
+++ index.js
@@ -6,35 +6,42 @@ const app = express();
 img = [];
 
 const scrap = async () => {
-    
-        const { data } = await axios.get('https://theinternship.io/');
-        let $ = cheerio.load(data);
+        try{
+            const { data } = await axios.get('theinternship.io/');
+            let $ = cheerio.load(data);
 
-        $('.center-logos').each(function(i,_){
-            if($(this)['0'] && $(this)['0'].attribs)
-                image = `${$(this)['0'].attribs.src}`;
-            img[i] = {img : image}
-        })
-        $('.list-company').each(function(i,_){
-            if($(this)['0'] && $(this)['0'].children[0])
-                text = `${$(this)['0'].children[0].data}`
-                img[i].size = text.length
-                img[i].data = text
-        })
-        img.sort((a,b) => a.size - b.size)
+            $('.center-logos').each(function(i,_){
+                if($(this)['0'] && $(this)['0'].attribs)
+                    image = `${$(this)['0'].attribs.src}`;
+                img[i] = {img : image}
+            })
+            $('.list-company').each(function(i,_){
+                if($(this)['0'] && $(this)['0'].children[0])
+                    text = `${$(this)['0'].children[0].data}`
+                    img[i].size = text.length
+                    img[i].data = text
+            })
+
+            img.sort((a,b) => a.size - b.size)
+    }catch(err){
+        throw new Error("Something went wrong.")
+    }
    
 };
 
 app.use('/',async (req,res,next)=>{
-    await scrap();
-    result = img.map(e =>  {
-        return {logo : `https://theinternship.io/${e.img}`}
-    });
-    res.status(200).send({
-        companies : result
-    })
+    try{
+        await scrap()
+        result = img.map(e =>  {
+            return {logo : `https://theinternship.io/${e.img}`}
+        });
+        res.status(200).send({
+            companies : result
+        })
+    }catch(err){
+        res.status(500).send(err.message)
+    }
 })
-
 
 app.listen(3000,()=>{
     console.log("Start server on port 3000")
